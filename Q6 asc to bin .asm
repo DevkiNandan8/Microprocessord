4feb7627f97a7af322b809b240c9393a4e2caf56
Devki Nandan <110334074+DevkiNandan8@users.noreply.github.com>
2023-12-07 22:09:56 +0530

    Q6 asc to bin .asm

--- Q6 asc to bin .asm
+++ Q6 asc to bin .asm
@@ -1,75 +1,59 @@
-.MODEL SMALL
-.DATA
-TEMP DW ?
-MES1 DB 13,10,10,"ENTER A NUMBER : $"
-MES2 DB 13,10,10,"THE NUMBER ENTERED IS : $"
+.model small
+.data
+input db 10,13,"Enter an ASCII character: $"
+output db 10,13,"The binary value is: $"
+binaryValue db 8 dup(?) ; To store the binary value
+.code
+.startup
+    mov ah, 09h
+    mov dx, offset input
+    int 21h
 
-.CODE
-.STARTUP
-MOV DX,OFFSET MES1
-MOV AH,9
-INT 21H
+    mov cx, 8 ; 8 bits for ASCII to binary conversion
 
-XOR AX,AX
-CALL READN
-MOV TEMP, AX
-MOV AH,9
-MOV DX, OFFSET MES2
-INT 21H
-MOV AX, TEMP
-CALL DISPX
-.EXIT
+    mov ah, 01h
+    int 21h ; Read a character from the user
 
-READN PROC NEAR
-    PUSH BX
-    PUSH CX
-    MOV CX,10
-    MOV BX, 0
-    READN1:
-        MOV AH,1
-        INT 21H
-        CMP AL,30H
-        JB READN2
-        CMP AL, 39H
-        JA READN2
-        SUB AL,30H
-        PUSH AX
-        MOV AX,BX
-        MUL CX
-        MOV BX,AX
-        POP AX
-        MOV AH,0
-        ADD BX,AX
-        JMP READN1
-        READN2:
-            MOV AX,BX
-            POP CX
-            POP BX
-            RET
-    READN ENDP
+    ; Convert the ASCII character to binary
+    sub al, 30h ; Subtract '0' to convert to a number (0-9)
+    cmp al, 1
+    jbe validInput ; Check if the input is a valid binary digit
 
-DISPX PROC NEAR
-    PUSH DX
-    PUSH CX
-    PUSH BX
-    MOV CX,0
-    MOV BX,10
-    DISPX1:
-        MOV DX,0
-        DIV BX
-        PUSH DX
-        INC CX
-        OR AX,AX
-        JNZ DISPX1   
-    DISPX2:
-        POP DX
-        MOV AH,2
-        ADD DL,30H
-        INT 21H
-        LOOP DISPX2
-   POP BX
-   POP CX
-   POP DX
-   RET
-DISPX ENDP
-END
+invalidInput:
+    ; Display an error message for invalid input
+    mov ah, 09h
+    mov dx, offset invalidMsg
+    int 21h
+    jmp endProgram
+
+validInput:
+    mov dl, al
+
+convertLoop:
+    test cx, 1 ; Check if we have processed 8 bits
+    jz skipBit ; If yes, skip this bit
+    mov al, 0 ; Clear AL to store the bit
+
+saveBit:
+    ; Store the bit in the binaryValue array
+    shl byte ptr [binaryValue], 1
+    or byte ptr [binaryValue], al
+
+skipBit:
+    shr cx, 1 ; Move to the next bit
+    loop convertLoop
+
+    mov ah, 09h
+    mov dx, offset output
+    int 21h
+
+    ; Display the binary value
+    mov ah, 09h
+    lea dx, binaryValue
+    int 21h
+
+endProgram:
+    mov ah, 4Ch
+    int 21h
+
+invalidMsg db 10,13,"Invalid input. Please enter a valid binary digit (0 or 1).$"
