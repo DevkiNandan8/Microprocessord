fc3551c6b7697bb8675c96b0fbf56c8ae97996c5
Devki Nandan <110334074+DevkiNandan8@users.noreply.github.com>
2023-12-07 21:34:53 +0530

    Q5 bin to asc .asm

--- Q5 bin to asc .asm
+++ Q5 bin to asc .asm
@@ -1,9 +1,52 @@
 .MODEL SMALL
+.DATA
+TEMP DW ?
+MES1 DB 13,10,10,"ENTER A NUMBER : $"
+MES2 DB 13,10,10,"THE NUMBER ENTERED IS : $"
+
 .CODE
 .STARTUP
-MOV AX,1234
+MOV DX,OFFSET MES1
+MOV AH,9
+INT 21H
+
+XOR AX,AX
+CALL READN
+MOV TEMP, AX
+MOV AH,9
+MOV DX, OFFSET MES2
+INT 21H
+MOV AX, TEMP
 CALL DISPX
 .EXIT
+
+READN PROC NEAR
+    PUSH BX
+    PUSH CX
+    MOV CX,10
+    MOV BX, 0
+    READN1:
+        MOV AH,1
+        INT 21H
+        CMP AL,30H
+        JB READN2
+        CMP AL, 39H
+        JA READN2
+        SUB AL,30H
+        PUSH AX
+        MOV AX,BX
+        MUL CX
+        MOV BX,AX
+        POP AX
+        MOV AH,0
+        ADD BX,AX
+        JMP READN1
+        READN2:
+            MOV AX,BX
+            POP CX
+            POP BX
+            RET
+    READN ENDP
 
 DISPX PROC NEAR
     PUSH DX
